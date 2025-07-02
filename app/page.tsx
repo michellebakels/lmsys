1d4f9efdff18d5c7822bad14b136a4743f4c3fd0
michellebakels <michelle.bakels@gmail.com>
2025-07-02 14:26:10 -0400

    feat: add bubble logo and adjust layout z-indices

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="min-h-screen p-8 bg-[#f0efeb]" style={{ fontFamily: 'var(--font-norb-pen)' }}>
+    <div
+      className="min-h-screen p-8 bg-[#f0efeb]"
+      style={{ fontFamily: "var(--font-norb-pen)" }}
+    >
       <div
         className="bg-[#f0efeb] h-[calc(100vh-4rem)] border-7 border-black overflow-hidden"
         style={{
@@ -14,7 +17,7 @@ export default function Home() {
           alt="Decorative bubbles"
           width={400}
           height={400}
-          className="absolute top-0 left-0 w-[300px] sm:w-[400px] h-auto"
+          className="absolute top-0 left-0 w-[300px] sm:w-[400px] h-auto z-0"
         />
         <div
           className="absolute inset-0"
@@ -34,10 +37,17 @@ export default function Home() {
           alt="Decorative bubbles"
           width={400}
           height={400}
-          className="absolute bottom-0 right-0 rotate-180 w-[300px] sm:w-[400px] h-auto"
+          className="absolute bottom-0 right-0 rotate-180 w-[300px] sm:w-[400px] h-auto z-0"
         />
-        <main className="flex flex-col gap-[32px] items-center sm:items-start font-norbpen text-6xl">
-          Developer Meetup
+        <Image
+          src="/bubble-logo.svg"
+          alt="Let Me Show You Something"
+          width={200}
+          height={200}
+          className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[200px] sm:w-[300px] h-auto z-20"
+        />
+        <main className="flex flex-col p-6 items-center sm:items-start font-norbpen text-6xl relative z-10">
+          <p>Developer Meetup</p>
         </main>
       </div>
     </div>
